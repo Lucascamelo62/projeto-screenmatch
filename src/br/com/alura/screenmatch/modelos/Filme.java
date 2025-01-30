9dd38cf6509973ca154760133f3ee2e69d6fc5f8
Lucascamelo62 <lucascamelo62@gmail.com>
2025-01-29 22:35:42 -0300

    Incluindo heranças e polimorfismo

--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -1,41 +1,13 @@
 package br.com.alura.screenmatch.modelos;
 
-public class Filme {
-    private String nomeFilme;
-    private int anoLancamento, duracaoMinuto, totalDeAvaliacao;
-    private boolean incluidoPlano;
-    private double somaDasAvaliacoes;
+public class Filme extends Titulo {
+    private String diretorFilme;
 
-    public int getTotalDeAvaliacao() {
-        return totalDeAvaliacao;
+    public String getDiretorFilme() {
+        return diretorFilme;
     }
 
-    public void setNomeFilme(String nomeFilme) {
-        this.nomeFilme = nomeFilme;
-    }
-
-    public void setAnoLancamento(int anoLancamento) {
-        this.anoLancamento = anoLancamento;
-    }
-
-    public void setDuracaoMinuto(int duracaoMinuto) {
-        this.duracaoMinuto = duracaoMinuto;
-    }
-
-    public void setIncluidoPlano(boolean incluidoPlano) {
-        this.incluidoPlano = incluidoPlano;
-    }
-
-    public void avaliaFilme(double nota) {
-        somaDasAvaliacoes += nota;
-        totalDeAvaliacao++;
-    }
-
-    public void exibeInformacao() {
-        System.out.println("Nome do filme: " + nomeFilme + "\n" + "Ano de Lançamento: " + anoLancamento + "\n" + "Soma das Avaliações: " + somaDasAvaliacoes + "\n" + "Total de Avaliações: " + totalDeAvaliacao);
-    }
-
-    public double calculaMediaAvaliacao() {
-        return somaDasAvaliacoes / totalDeAvaliacao;
+    public void setDiretorFilme(String diretorFilme) {
+        this.diretorFilme = diretorFilme;
     }
 }
